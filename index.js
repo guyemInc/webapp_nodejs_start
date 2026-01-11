e0f7a1b0c973f54538b229e19bde939e4a8a8341
Malinge <g.malinge@groupeginger.com>
2026-01-11 22:23:49 +0100

    First commit projet nodejs simple

---
+++ index.js
@@ -0,0 +1,7 @@
+/* console.log("Hello world");  */
+const express = require('express');
+const app = express();
+app.use(express.static('public/'));
+app.listen(8099), function(){
+	console.log("Hello world at 8080");
+};
